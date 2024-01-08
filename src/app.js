e52e93b0a3fb2146366d459573a01b3b8cdf2e95
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-01-08 13:52:10 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,4 +13,15 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes
+import userRouter from './routes/user.route.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+//we use this route as - http://localhost:8000/api/v1/users/register
+
+
+
 export default app
