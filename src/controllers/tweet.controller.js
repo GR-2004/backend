8d8db0db9e0c08d943ebe52fdbd1cc68991358fe
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-01-19 10:38:34 +0530

    add tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,18 +7,78 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    try {
+        const {content} = req.body
+        if(!content){
+            throw new ApiError(400, "please insert content of tweet")
+        }
+        const userId = req.user?._id
+        const tweet = await Tweet.create({
+            content,
+            owner: mongoose.Types.ObjectId(userId)
+        })
+        return res
+        .status(200)
+        .json(new ApiResponse(200, {tweet}, "tweet created successfully"))
+    } catch (error) {
+        throw new ApiError(500, error.message || "Something went wrong, while creating a tweet")
+    }
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    try {
+        const userId = req.user?._id
+        const tweet = await Tweet.find({
+            owner: mongoose.Types.ObjectId(userId)
+        })
+        return res
+        .status(200)
+        .json(new ApiResponse(200, {tweet}, "All tweets fetched successfully"))
+    } catch (error) {
+        throw new ApiError(500, error.message || "Something went wrong, while fetching all tweets")
+    }
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const { tweetId } = req.params
+    const { content } = req.body
+    try {
+        const tweet = await Tweet.findByIdAndUpdate(
+            {
+                _id : mongoose.Types.ObjectId(tweetId)
+            },
+            {
+                content
+            },
+            {
+                new: true
+            }
+        )
+        return res
+        .status(200)
+        .json(new ApiResponse(200,{tweet}, "tweet updated successfully"))
+    } catch (error) {
+        throw new ApiError(500, `Something went wrong, while updating the tweets ${error.message}`)
+    }
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const { tweetId } = req.params
+    try {
+        const tweet = await Tweet.deleteOne(
+            {
+                _id : mongoose.Types.ObjectId(tweetId)
+            },
+        )
+        return res
+        .status(200)
+        .json(new ApiResponse(200, {tweet}, "tweet deleted successfully"))
+    } catch (error) {
+        throw new ApiError(500, `Something went wrong, while deleting the tweet ${error.message}`)
+    }
 })
 
 export {
